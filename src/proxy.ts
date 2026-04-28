0f6031ca804d6970b9c2c2745aed9cae59163709
loai-osama-aka <loai.osa5050@gmail.com>
2026-04-29 00:59:09 +0300

    proxy fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,23 +1,26 @@
-import { getToken } from 'next-auth/jwt'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function proxy(request: NextRequest) {
-    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
-    console.log(token, 'tokkkkkkkkkken');
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
 
-    if (token?.token) {
-        return NextResponse.next()
-    } else {
+  console.log("MIDDLEWARE TOKEN:", token);
 
-        return NextResponse.redirect(new URL('/auth/login', request.url))
-    }
+  const isAuthenticated = !!token;
 
+  if (!isAuthenticated) {
+    return NextResponse.redirect(
+      new URL("/auth/login", request.url)
+    );
+  }
 
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/cart','/allorders'],
-}
-
-
+  matcher: ["/cart", "/allorders"],
+};
