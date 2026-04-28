5d24ad4f0b9db6b6a173df86c8372f4922adbe35
loai-osama-aka <loai.osa5050@gmail.com>
2026-04-29 02:50:31 +0300

    commit

--- src/app/checkout/CheckoutClient.tsx
+++ src/app/checkout/CheckoutClient.tsx
@@ -365,7 +365,6 @@ export default function CheckoutClient({
               onClick={handleCheckout}
               className="w-full mt-6 bg-linear-to-r from-green-600 to-green-700 text-white py-4 rounded-xl font-bold hover:from-green-700 hover:to-green-800 transition-all flex items-center justify-center gap-2 shadow-lg active:scale-[0.98]"
             >
-              <FaBox /> Place Order
               {isloading ? (
                 <FaSpinner className="animate-spin" />
               ) : (
